475349755256bd66050c73ed740a23d12d4646a7
李東穎 <dean19990226@gmail.com>
2022-09-04 20:48:14 +0800

    Update 2022-09-04T12:47:14.409Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -14,9 +14,10 @@ $(document).ready(function () {
   }); // scoll end
   //  漢堡選單 start
 
-  $('.btn-buger').click(function (e) {
-    $('.nav-content_main').toggleClass('open');
-    $('.btn-buger').toggleClass('open');
+  $('.btn-burger').click(function (e) {
+    $('.nav__content__main').toggleClass('open');
+    $('.nav__content__main a').toggleClass('open');
+    $('.btn-burger').toggleClass('open');
   }); //  漢堡選單 end
   //點小圖換大圖 start
 
