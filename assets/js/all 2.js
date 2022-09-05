49654fef45f8d501770a582db98e7a60cfc4c382
李東穎 <dean19990226@gmail.com>
2022-09-06 04:06:12 +0800

    Update 2022-09-05T20:06:09.328Z

--- assets/js/all 2.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// loading 動畫start
-$(window).on('load', function () {
-  $('.loading-overlay').addClass('active');
-}); // loading 動畫end
-
-$(function () {});
-//# sourceMappingURL=all.js.map
