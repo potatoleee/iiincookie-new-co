1d2430eeec9c26c9b821ac093129c18438aa152c
李東穎 <dean19990226@gmail.com>
2022-09-08 11:18:10 +0800

    Update 2022-09-08T03:18:03.199Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,40 @@
 "use strict";
 
 // new WOW().init();
-AOS.init(); // loading 動畫start
+AOS.init({
+  // Global settings:
+  disable: false,
+  // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded',
+  // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init',
+  // class applied after initialization
+  animatedClassName: 'aos-animate',
+  // class applied on animation
+  useClassNames: false,
+  // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false,
+  // disables automatic mutations' detections (advanced)
+  debounceDelay: 50,
+  // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99,
+  // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 150,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 1000,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease',
+  // default easing for AOS animations
+  once: false,
+  // whether animation should happen only once - while scrolling down
+  mirror: false,
+  // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+}); // loading 動畫start
 
 $(window).on('load', function () {
   $('.loading-overlay').addClass('active');
@@ -12,7 +45,7 @@ $(document).ready(function () {
   $('.pagetop').on('click', function () {
     $('html,body').animate({
       scrollTop: 0
-    });
+    }, 1000);
   }); // scoll end
   //  漢堡選單 start
 
@@ -53,7 +86,7 @@ $(document).ready(function () {
   function gsapAnimate() {
     if ($(window).width() >= 768) {
       $(window).on('scroll', function () {
-        offsetTop = $(window).scrollTop(); // console.log(offsetTop/10)
+        var offsetTop = $(window).scrollTop(); // console.log(offsetTop/10)
         //x:“偏移”,“時間：數字越大呈現時間越久"
 
         gsap.to(".about-text", {
@@ -62,8 +95,8 @@ $(document).ready(function () {
         });
       }); //scroll end
     } else {
-      $(window).scroll(function () {
-        offsetTop = $(window).scrollTop(); // console.log(offsetTop/10)
+      $(window).on('scroll', function () {
+        var offsetTop = $(window).scrollTop(); // console.log(offsetTop/10)
         //x:“偏移”,“時間：數字越大呈現時間越久"
 
         gsap.to(".about-text", {
